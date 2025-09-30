5a747fa3a6fc30f13484856da11e2cb40ec050eb
farzanacoder <farzanaashif10@gmail.com>
2025-09-30 12:59:30 +0600

    some update

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -37,7 +37,7 @@ const Faq = () => {
   }
 
   return (
-    <div className="max-w-4xl mx-auto px-4 lg:px-8 py-10">
+    <div className="max-w-4xl mx-auto px-4 md:px-8 lg:px-8 py-10">
       <h2 className="text-2xl md:text-[32px] lg:text-5xl font-bold text-[#1B1743] text-center mb-6">
         Frequently asked questions
       </h2>
